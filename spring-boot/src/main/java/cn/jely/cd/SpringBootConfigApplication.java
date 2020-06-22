13e69d8070420d6effb9269d1347c242fd4524c1
zhou <623109799@qq.com>
2020-06-22 13:36:29 +0800

    add spring-boot-sample

---
+++ spring-boot/src/main/java/cn/jely/cd/SpringBootConfigApplication.java
@@ -0,0 +1,8 @@
+package cn.jely.cd;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+    public static void main(String[] args) {
+        Spring
+    }
+}
